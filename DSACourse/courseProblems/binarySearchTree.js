6e6537ab43f22ca4469e8ffe10d2c87d9c39b1ca
mehuledneed <mehul.edneed@gmail.com>
2022-11-09 18:23:50 +0530

    bst find

--- DSACourse/courseProblems/binarySearchTree.js
+++ DSACourse/courseProblems/binarySearchTree.js
@@ -35,6 +35,19 @@ class BinarySearchTree {
       }
     }
   }
+
+  find(value) {
+    if (!this.root) return undefined;
+    if (this.root.value === value) return this.root;
+    let currNode = this.root;
+    let found = false;
+    while (currNode && !found) {
+      if (currNode.value > value) currNode = currNode.left;
+      else if (currNode.value < value) currNode = currNode.right;
+      else found = true;
+    }
+    return found ? currNode : undefined;
+  }
 }
 
 var binarySearchTree = new BinarySearchTree();
@@ -49,3 +62,21 @@ var binarySearchTree = new BinarySearchTree();
 // console.log(binarySearchTree.root.value) // 15
 // console.log(binarySearchTree.root.right.value) // 20
 // console.log(binarySearchTree.root.left.right.value) // 12
+
+// binarySearchTree
+//   .insert(15)
+//   .insert(20)
+//   .insert(10)
+//   .insert(12);
+// var foundNode = binarySearchTree.find(20);
+// console.log(foundNode.value) // 20
+// console.log(foundNode.left) // null
+// console.log(foundNode.right) // null
+
+// binarySearchTree
+//   .insert(15)
+//   .insert(20)
+//   .insert(10)
+//   .insert(12);
+// var foundNode = binarySearchTree.find(120);
+// console.log(foundNode); // undefined
